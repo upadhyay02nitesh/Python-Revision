bde842cbe3a44ae9ddee6d6f5245a382be5d2361
Nitesh Upadhyay <02nitesh@gmail.com>
2025-08-31 21:18:59 +0530

    add

--- Matrix_Problem/main.py
+++ Matrix_Problem/main.py
@@ -15,6 +15,7 @@
 A = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
+
 m=len(A)
 
 B = [[9, 8, 7],
@@ -58,13 +59,78 @@ n=len(B)
 
 
 # 🔹 4. Matrix Transpose (rows → columns)
+# A = [[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 9]]
+
+# transpose = [[0]*3 for _ in range(3)]
+
+# for i in range(len(A)):
+#     for j in range(len(A[i])):
+#         transpose[i][j]=A[j][i]
+# print(transpose)
+
+
+# 5. Diagonal Elements
+# A = [[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 9]]
+
+# n=len(A)
+# s=[]
+
+# for i in range(len(A)):
+#      s.append(A[i][i])
+#      m+=1
+# print(s)
+     
+     
+     
+     
+# 6. Rotate Matrix 90° Clockwise
+
+# 👉 Rule: Transpose + Reverse each row
+
+# A = [[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 9]]
+
+# [7, 4, 1]
+# [8, 5, 2]
+# [9, 6, 3]
+
+# transpose = [[0]*3 for _ in range(3)]
+
+# for i in range(len(A)):
+#     for j in range(len(A[i])):
+#         transpose[i][j]=A[j][i]
+
+
+# for i in range(len(transpose)):
+#      transpose[i]=transpose[i][::-1]
+# print(transpose)
+
+
+
+# 🔹 7. Rotate Matrix 90° Anti-Clockwise
+
+# 👉 Rule: Transpose + Reverse each column
+
+
 A = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
+
+# [3, 6, 9]
+# [2, 5, 8]
+# [1, 4, 7]
+
 
 transpose = [[0]*3 for _ in range(3)]
 
 for i in range(len(A)):
     for j in range(len(A[i])):
         transpose[i][j]=A[j][i]
+
+transpose=transpose[::-1]
 print(transpose)
